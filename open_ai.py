68c9b7464385294fabf714c59351c48d1390c716
takatama <takatama@users.noreply.github.com>
2024-01-21 17:04:48 +0900

    create_quizを追加

--- open_ai.py
+++ open_ai.py
@@ -4,17 +4,18 @@ from openai import OpenAI
 
 load_dotenv()
 
-client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY"),
-)
+def create_quiz(genre="Python", num_questions=3, num_options=4):
+    client = OpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
+    response = client.chat.completions.create(
+        model=os.getenv("OPENAI_MODEL"),
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant. You can create a quiz about anything. 日本語で回答してください。lang:ja"},
+            {"role": "user", "content": f"I want {num_questions} questions with {num_options} answer options about {genre}."}
+        ]
+    )
+    return response.choices[0].message.content
 
-response = client.chat.completions.create(
-  model=os.getenv("OPENAI_MODEL"),
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Who won the world series in 2020?"},
-    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-    {"role": "user", "content": "Where was it played?"}
-  ]
-)
-print(response.choices[0].message.content)
+if __name__ == "__main__":
+    print(create_quiz())
